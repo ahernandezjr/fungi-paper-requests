1eb861407a8b683bbe74e5263586da966ca0d834
ahernandezjr <76761720+ahernandezjr@users.noreply.github.com>
2022-02-01 02:29:04 -0600

    Unstable build... Added get_papers_from_list, get_index, get_array, and sanitize_text.

--- fungi-paper-retrieval.py
+++ fungi-paper-retrieval.py
@@ -1,9 +1,12 @@
 from posixpath import dirname
 import shutil
 import os
+from time import sleep
+import numpy as np
+import re
 import requests
 import xml.etree.ElementTree as ET
-from time import sleep
+import json
 
 # Retrieving genus list
 # WARNING: Only used the first time
@@ -20,29 +23,32 @@ def import_init_list(origin_file_name, new_file_name):
     origin_array.sort()
 
     # Create new "remainder" file
-    with open(new_file_name, "w") as f:
-        for row in origin_array:
-            f.write(row + "\n")
+    np.savetxt(new_file_name, origin_array, fmt="%s")
 
     source = new_file_name
-    destination = "backups/" + new_file_name
+    destination = "backups/" + new_file_name.split('/')[1]
     shutil.copy(source, destination)
+
+
+# Create an array from a file
+# @params: file = file to pull array from
+def get_array(file):
+    array = []
+    with open(file, mode='r', encoding='utf-8') as array_file:
+        array = array_file.read().splitlines()
+
+    return array
+
 
 # Create index files with ID's for all papers of a genus
 # @params: query = genus-to-be-searched
-def get_ids(list_file):
+def get_ids(query_list):
     # First and second parts of the url
     BASE_URL1 = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pmc&term='
     BASE_URL2 = '&retmax=1000000'
 
-    # Obtain Query List for folders and indexes
-    query_list = []
-    with open(list_file) as f:
-        for row in f:
-            query_list.append(row.strip())
-
-    # 
-    for query in query_list[2570:]:
+    # Start of looping through genus_list to get all ids
+    for query in query_list:
         sleep(0.35)
         query = query.strip()
 
@@ -66,10 +72,11 @@ def get_ids(list_file):
             # Response is stored into a local xml file to process and view
             with open("temp.xml", "w") as temp:
                 temp.write(response.text)
-            # Just a test file to see content-type
-            with open("test.xml", "w") as test:
-                test.write(response.headers.get('Content-Type'))
-            
+
+            # DEBUG: Just a test file to see content-type
+                # with open("test.xml", "w") as test:
+                #     test.write(response.headers.get('Content-Type'))
+                
             # Transforms data to be parsed by xml
             tree = ET.parse("temp.xml")
             root = tree.getroot()
@@ -86,14 +93,12 @@ def get_ids(list_file):
             for id in root.findall('IdList/Id'):
                 f.write(id.text + "\n")
 
-def compute_total_ids(genus_list):
-    total = 0
-    genus_array = []
 
-    with open(genus_list, mode='r', encoding='utf-8') as genus_file:
-        # Create array of genus list from the file
-        for genus in genus_file:
-            genus_array.append(genus.strip())
+# Compute total number of IDs found for data analysis
+# @params: genus_list = list of the genus to view index files
+def compute_total_ids(genus_array):
+    # Variable for sum
+    total = 0
 
     with open('metadata/genus_id_totals.txt', 'w') as id_file:
         for genus in genus_array:
@@ -104,28 +109,188 @@ def compute_total_ids(genus_list):
             # Gets total ID's in file and does calculations and writing to total file
             this_total = sum(1 for line in open(dirname + '/' + genus + '_index.txt'))
             total += this_total
-            print(total)
             id_file.write(genus + ':' + str(this_total) + '\n')
         
         id_file.write('Total: ' + str(total) + '\n')
+    
+
+# Gets index of a genus from its folder/file
+# @params: query_list = list of genuses (queries)) to be searched and data pulled
+def get_index(query):
+    filename = 'data/' + query + '/'
+    dirname = os.path.dirname(filename)
+    index_file_location = dirname + '/' + query + '_index.txt'
+
+    # Takes all IDs from index of query
+    with open(index_file_location) as index_file:
+        index_array = index_file.read().splitlines()
+
+    return index_array
+
+
+# Get papers from a list of genuses to be searched
+# @params: query = genus, index_array = list of ID's to get papers from
+def get_papers_from_list(query, index_array):
+    print("This array has a length of " + str(len(index_array)))
+
+    # Make Directory if !exists
+    filename = "data/" + query + "/"
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+    BASE_URL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pmc&id='
+    url_append = ','.join(index_array)
+    search_url = BASE_URL + url_append
+
+    # Gets response 
+    response = requests.get(search_url)
+    root = ET.fromstring(response.text)
+
+    # Inner for loop for adding papers from requests
+    if(len(index_array) == 1):
+        # Determine ID with preference for PMC
+        id = None
+        pmc = None
+        pmid = None
+        doi = None
+        for child in root.findall('.//front/article-meta/article-id'):
+            id = sanitize_text(child.text)
+            if(child.get('pub-id-type') == 'pmc'):
+                pmc = sanitize_text(child.text)
+            elif(child.get('pub-id-type') == 'pmid'):
+                pmid = sanitize_text(child.text)
+            elif(child.get('pub-id-type') == 'doi'):
+                doi = sanitize_text(child.text)
+        if(pmc): id = pmc
+        elif(pmid): id = pmid
+        elif(doi): id = doi
+
+        # Converting body to proper format
+        text = sanitize_text(ET.tostring(root.find('.//body'), encoding='utf-8', method='text').decode('utf-8'))
+        text = re.sub(r"\s{2,}", " ", text.replace('\n', ' ').replace('\r', ' ').strip())
+
+        # Json Array that holds the title, id, and text
+        query_array = {
+            "id": id,
+            "genus": query,
+            "text": text
+        }
+
+        # Creating file
+        with open(dirname + "/" + query + "_" + id + ".json", "w") as f:
+            f.write(json.dumps(query_array, indent=3))
+            
+    else:
+        # IMPORTANT --------------------------------------
 
 
 
 
-# Pull all papers from index for genus
-# def get_papers(genus_name):
-    # Request of paper and writing to file
-    # file = open("test.xml", "w")
-
-    # response = requests.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pmc&id=8779134')
-    # file.write(response.text)
-    # file.close("test.xml")
+        # Add situation where this message appears and the front is only seeable:
+        # <!-- The publisher of this article does not allow downloading of the full text in XML form. -->
+        # IDEAS: check length of 'article'
 
 
-# Import First List to New List to Count Remainder
+
+
+
+        # IMPORTANT --------------------------------------
+
+        x = 0
+        for article in root.findall('article'):
+            x+=1
+            print("article: " + str(x))
+            # Determine ID with preference for PMC
+            id = None
+            pmc = None
+            pmid = None
+            doi = None
+            for child in article.findall('./front/article-meta/article-id'):
+                id = sanitize_text(child.text)
+                if(child.get('pub-id-type') == 'pmc'):
+                    pmc = sanitize_text(child.text)
+                elif(child.get('pub-id-type') == 'pmid'):
+                    pmid = sanitize_text(child.text)
+                elif(child.get('pub-id-type') == 'doi'):
+                    doi = sanitize_text(child.text)
+            if(pmc): id = pmc
+            elif(pmid): id = pmid
+            elif(doi): id = doi
+
+            # Converting body to proper format
+            text = sanitize_text(ET.tostring(article.find('./body'), encoding='utf-8', method='text').decode('utf-8'))
+            text = re.sub(r"\s{2,}", " ", text.replace('\n', ' ').replace('\r', ' ').strip())
+
+            # Json Array that holds the title, id, and text
+            query_array = {
+                "id": id,
+                "genus": query,
+                "text": text
+            }
+
+            # Creating file
+            with open('article' + str(x) + '.json', 'w', encoding='utf-8') as f:
+                f.write(json.dumps(query_array, indent=3))
+    
+    return
+
+
+def sanitize_text(text):
+    text = re.sub(r"\s{2,}|(\n+)|(\r+)|(\t+)", " ", text)
+    return text
+
+
+
+
+# ------------------------------------------------------------
+# ------------------------------------------------------------
+# STEP 1 - Import First List to New List to Count Remainder
     # import_init_list("metadata/genus_final_list.txt", "metadata/genus_remainder.txt")
 
-# Get ID's using list
-    # get_ids("metadata/genus_remainder.txt")
+# STEP 2 - Get ID's using list
+    # get_ids(get_array('metadata/genus_remainder.txt'))
 
-compute_total_ids('metadata/genus_remainder.txt')
+# OPTIONAL - Generate data from IDs 
+    # compute_total_ids(get_array('metadata/genus_remainder.txt'))
+
+# STEP 3 - Get Papers from IDs
+
+# Outer For Loop for going through the query list
+# for x in range(len(job_array)):
+query_array = []
+with open('metadata/genus_remainder.txt') as f:
+    query_array = f.read().splitlines()
+
+
+# for query in query_array:
+# query ='aabaarnia'
+query = 'Aaosphaeria'
+get_papers_from_list(query, get_index(query))
+
+    # query = query_array.pop(0)
+
+
+    # If less than 100 ID's, request once
+    # ADD ------ IF only 1 article, it needs to be a level higher
+    
+
+    # else:
+    #     for x in index_array:
+    #         response = requests.get()
+    #         file.write(response.text)
+    #         file.close("test.xml")
+    
+    
+        # Search for articles with item
+        # Check how many ids are in item index
+
+        
+    # ENABLE ONCE DONE TESTING
+    # Re-writes the job_file to update it
+    # with open(job_list, 'w') as f:
+    #     f.writelines(job_array)
+
+
+# ------------------------------------------------------------
+# ------------------------------------------------------------
